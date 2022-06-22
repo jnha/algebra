7622765e9058eb56f66d738013015f734c565a32
James Hally <32179923+jnha@users.noreply.github.com>
2022-06-22 11:19:39 -0400

    Rename Polynomial to Univariate to allow for a more flexible polynomial class in the future

--- algebra/polynomial.py
+++ algebra/polynomial.py
@@ -3,24 +3,67 @@ from __future__ import annotations
 from itertools import zip_longest
 from typing import Generic, TypeVar
 
-R = TypeVar('R')
+R = TypeVar('R')  # Type of coefficients
+E = TypeVar('E')  # Type of exponents
 
 
-class Polynomial(Generic[R]):
-    """Univariate polynomial"""
+class Polynomial(Generic[R, E]):
+    """Generic Polynomials
+
+    Implimented as a map from exponents to coefficients.
+    Allows multivariate polynomials via direct products of exponents,
+    and non integer coefficients as long as __add__ and __sub__ are implimented
+    """
+    pass  # This is more complicated, impliment later
+
+
+class Univariate(Generic[R]):
+    """Univariate polynomials with integer exponents
+
+    implimented as a tuple
+    """
     def __init__(self, *coefficients: R) -> None:
-        end = 1 + max((i for i, c in enumerate(coefficients) if c), default=-1)
+        end = next((len(coefficients)-i
+                    for i, c in enumerate(reversed(coefficients)) if c),
+                   -1)
         self.coeffs = coefficients[:end]
 
     def degree(self) -> int:
         """Return the degree of the polynomial
 
-        >>> Polynomial(1, 0, 1).degree()
+        >>> Univariate(1, 0, 1).degree()
         2
         """
         return len(self)-1
 
-    # -- Magic Methods --
+    # -- String representations --
+    def __repr__(self):
+        return 'Univariate('+', '.join(repr(c) for c in self)+')'
+
+    def __str__(self):
+        if not self:
+            return '0'
+        return '-' if self[-1] < 0 else '' + ''.join(reversed(
+            ' - ' if c < 0 else ' + '
+            + str(abs(c)) if abs(c) != 1 else ''
+            + '' if i == 0 else 'x' if i == 1 else f'x**{i}'
+            for i, c in enumerate(self) if c)[3:]
+        )
+
+    # -- Equality --
+    def __eq__(self, other):
+        """
+        >>> Univariate(1, 2, 3) == Univariate(1, 2, 3)
+        True
+        >>> Univariate(1, 2, 4) == (1, 2, 4)
+        True
+        """
+        return self.coeffs == other
+
+    def __hash__(self):
+        return hash(self.coeffs)
+
+    # -- Container dunders --
     def __len__(self) -> int:
         return len(self.coeffs)
 
@@ -33,34 +76,39 @@ class Polynomial(Generic[R]):
         return iter(self.coeffs)
 
     def __reversed__(self):
-        return reversed(self.coefficients)
+        """
+        >>> list(reversed(Univariate(1, 2, 3)))
+        [3, 2, 1]
+        """
+        return reversed(self.coeffs)
 
-    def __eq__(self, other):
-        if not hasattr(other, 'coeffs'):
-            return NotImplemented
-        return self.coeffs == other.coeffs
+    # -- Arithmetic operations --
+    def __neg__(self) -> Univariate[R]:
+        return Univariate(*(-x for x in self))
 
-    def __hash__(self):
-        return hash(self.coeffs)
-
-    def __repr__(self):
-        if not self:
-            return '0'
-        return '-' if self[-1] < 0 else '' + ''.join(reversed(
-            ' - ' if c < 0 else ' + '
-            + str(abs(c)) if abs(c) != 1 else ''
-            + '' if i == 0 else 'x' if i == 1 else f'x**{i}'
-            for i, c in enumerate(self) if c)[3:]
-        )
-
-    def __neg__(self) -> Polynomial[R]:
-        return Polynomial(*(-x for x in self))
-
-    def __add__(self, other) -> Polynomial[R]:
-        if not hasattr(other, 'coeffs'):
-            return NotImplemented
-        return Polynomial(*(s+o for s, o
+    def __add__(self, other) -> Univariate[R]:
+        return Univariate(*(s+o for s, o
                           in zip_longest(self, other, fillvalue=0)))
 
-    def __mul__(self, other) -> Polynomial[R]:
-        return NotImplemented
+    def __radd__(self, other) -> Univariate[R]:
+        return self + other
+
+    def __mul__(self, other) -> Univariate[R]:
+        out = [0] * (len(self) + len(other))
+        for i, s in self:
+            for j, o in other:
+                out[i+j] += s*o
+        return Univariate(*out)
+
+    def __rmul__(self, other) -> Univariate[R]:
+        out = [0] * (len(self) + len(other))
+        for i, s in self:
+            for j, o in other:
+                out[i+j] += o*s
+        return Univariate(*out)
+
+    def __lshift__(self, i):
+        return Univariate(*(0,)*i, *self)
+
+    def __rshift__(self, i):
+        return Univariate(*self[i:])
